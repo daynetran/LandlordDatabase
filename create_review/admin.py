947b76595761cc2f9e6f3e9c505f8520cb708269
Karson Du <karsondu@berkeley.edu>
2022-10-26 20:04:04 -0700

    Models & Admin

--- create_review/admin.py
+++ create_review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import LandLords, home, user
+
+admin.site.register(Landlords)
+admin.site.register(home)
+admin,site.register(user)
 
 # Register your models here.
--- create_review/models.py
+++ create_review/models.py
@@ -3,28 +3,48 @@ class LandLords (models.model):
     first_name = models.CharField(max_length-50)
     last_name = models.CharField(max_length-50)
     ratingOfLord = models.IntegerField()
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+    def get_absolute_url(self):
+        return reverse('landlord-detail', args=[str(self.id)])
 
 class ReviewsOfLandLords (models.Model):
     dateOfReview = models.DateField()
     textOfReview = models.CharField(max_length-500)
-    reviewOfLord = model.ForeignKey(Home)
-    typingReview = null
+    reviewOfLord = models.ForeignKey(LandLords)
+    typingReview = models.TextField()
+
+
 
 class ReviewsOfHome (models.Model):
     dateOfReview = models.DateField()
     textOfReview = models.CharField(max_length-500)
-    reviewOfLord = model.ForeignKey(LandLords)
-    typingReview = null
+    reviewOfLord = models.ForeignKey(home)
+    typingReview = models.TextField()
 
 class photosForReviews (models.Model):
-    Review = models.ForeignKey(Reviews)
+    Review = models.ForeignKey(home)
     datePhotos = models.DateField()
 
 class home ():
     address = models.CharField(max_length-250)
     ratingOfHome = models.IntegerField()
-    reviewOfHome = model.ForeignKey(LandLords)
+    reviewOfHome = models.ForeignKey(LandLords)
+    class Meta:
+        ordering = ['address']
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+    def __str__(self):
+        return f'{self.address}'
 
 class user (models.Model):
     FirstName = models.CharField(max_length-50)
     LastName = models.CharField(max_length-50)
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.id)])
